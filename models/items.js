d8080f632d096342d8d309a89e96c77a8d1d1916
PzOng <pangszee.ong@gmail.com>
2020-05-27 00:13:58 +0930

    models & index html

---
+++ models/items.js
@@ -0,0 +1,48 @@
+module.exports = function(sequelize, DataTypes){
+    const item = sequelize.define("item",{
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[1]
+            }
+        },
+        details: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        price: {
+            type: DataTypes.DOUBLE,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        current: {
+            type: DataTypes.BOOLEAN,
+            defaulValue: true
+        }
+
+    });
+
+    item.associate = function(models){
+        item.belongsTo(models.group,{
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    item.associate = function(models){
+        item.belongsTo(models.orders,{
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return item
+};
---
+++ models/user.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes){
+    const user = sequelize.define("user",{
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[1]
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        current: {
+            type: DataTypes.BOOLEAN,
+            defaulValue: true
+        }
+
+    });
+
+    user.associate = function(models){
+        catetgory.hasMany(models.orders, {
+        })
+    };
+
+    return user
+};
